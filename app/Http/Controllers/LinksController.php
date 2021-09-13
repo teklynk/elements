07ec9125bfd6e0770102f56aea65a90e02f89eb4
teklynk <ryanjones153@gmail.com>
2021-09-13 17:19:17 -0400

    added links page WIP

---
+++ app/Http/Controllers/LinksController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Links;
+use Illuminate\Http\Request;
+
+class LinksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user_id = auth()->id();
+
+        $links = Links::orderBy('link_sort', 'desc')->where('user_id', $user_id)->get();
+        return view('links.index')->with('links', $links);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $user_id = auth()->id();
+
+        if (is_null($user_id)) {
+            return abort(404);
+        }
+
+        //$links = Links::orderBy('link_sort', 'desc')->where('user_id', $user_id)->get();
+        //return view('links.create')->with('links', $links);
+
+        return view('links.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Links  $links
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Links $links)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Links  $links
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Links $links)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Links  $links
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Links $links)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Links  $links
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Links $links)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_06_01_222521_create_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('link_name');
+            $table->string('link_url');
+            $table->string('link_thumbnail');
+            $table->integer('link_sort');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('links');
+    }
+}
