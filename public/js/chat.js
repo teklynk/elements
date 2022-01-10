4a403c2888c4c8197f3e726eb3be5444c954763b
teklynk <ryanjones153@gmail.com>
2022-01-10 14:25:30 -0500

    updated to v2 emoticons

--- public/js/chat.js
+++ public/js/chat.js
@@ -141,7 +141,7 @@ function formatEmotes(text, emotes) {
                     return '';
                 });
                 splitText = splitText.slice(0, mote[0]).concat(empty).concat(splitText.slice(mote[1] + 1, splitText.length));
-                splitText.splice(mote[0], 1, '<img class="emoticon" src="https://static-cdn.jtvnw.net/emoticons/v1/' + i + '/3.0">');
+                splitText.splice(mote[0], 1, '<img class="emoticon" src="https://static-cdn.jtvnw.net/emoticons/v2/' + i + '/default/dark/3.0">');
             }
         }
     }
